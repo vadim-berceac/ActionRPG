11b33a1e26ef7c84de1baaf915933965857be318
v.berchak <v.berchak@nekki.com>
2026-06-02 14:51:37 +0300

    init

---
+++ Assets/Game/Scripts/Game/Helpers/FixedUpdateFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gamekit3D
+{
+    [DefaultExecutionOrder(9999)]
+    public class FixedUpdateFollow : MonoBehaviour
+    {
+        public Transform toFollow;
+
+        private void FixedUpdate()
+        {
+            transform.position = toFollow.position;
+            transform.rotation = toFollow.rotation;
+        }
+    } 
+}
