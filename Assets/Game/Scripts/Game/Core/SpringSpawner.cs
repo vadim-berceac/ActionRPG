70c7e984c76dd423b658677da2f224ab57d7889b
v.berchak <v.berchak@nekki.com>
2026-08-22 09:03:07 +0300

    spring

---
+++ Assets/Game/Scripts/Game/Core/SpringSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SpringSpawner : MonoBehaviour
+{
+    [field: SerializeField] public SpringSpawnerData SpringSpawnerData { get; set; }
+    
+    private CharacterSpring _spawnedObject;
+
+    private void Awake()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        _spawnedObject = Instantiate(SpringSpawnerData.SpringPrefab,
+            SpringSpawnerData.CharacterTransform.position, Quaternion.identity).GetComponent<CharacterSpring>();
+        
+        _spawnedObject.Initialize(SpringSpawnerData.CharacterTransform,
+            SpringSpawnerData.DeformationBodyTransform);
+    }
+}
+
+[System.Serializable]
+public struct SpringSpawnerData
+{
+    [field: SerializeField] public GameObject SpringPrefab { get; set; }
+    [field: SerializeField] public Transform CharacterTransform { get; set; }
+    [field: SerializeField] public Transform DeformationBodyTransform { get; set; }
+}
